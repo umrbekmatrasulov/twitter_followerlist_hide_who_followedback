7bc14dce0256a22e715645b79d2d6e9e927df4f9
umrbekmatrasulov <82716960+umrbekmatrasulov@users.noreply.github.com>
2022-05-16 13:27:20 +0300

    Create js_twitter_hide_followers_who_followed_back.js

---
+++ js_twitter_hide_followers_who_followed_back.js
@@ -0,0 +1,17 @@
+var userContainers = document.querySelectorAll('[data-testid=UserCell]');
+for (var i = 0; i < userContainers.length; i++) {
+    var myf = function(){
+        try{
+            if (userContainers[i].childNodes[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0].innerHTML=='Follows you') {
+                userContainers[i].style.display = "none";
+            } else {
+                userContainers[i].style = "background-color: green !important;";
+            }
+        }catch(e){
+
+        }
+    };
+    myf();
+
+    
+};
